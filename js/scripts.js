827772dab28325617dd705ebe5af178f17a4d6db
sewest7 <seth.west7@gmail.com>
2022-01-12 16:19:02 -0800

    Add form with entries for name, food, music, and color dquote> dquote> Co-Authored-by: Michael Shamrell <michaelshamrell2001@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,14 @@ $(document).ready(function() {
   $("#formOne").submit(function(event) {
     event.preventDefault();
     const nameInput = $("input#name").val(); 
-    const food = $("#food").val();
-    const music = $("input:radio[name=music]:checked").val();
-    const favoriteColor = $("#color").val();
+    const foodInput = $("#food").val();
+    const musicInput = $("input:radio[name=music]:checked").val();
+    const colorInput = $("#color").val();
 
     $(".name").append(nameInput);
-    $(".food").append();
+    $(".food").append(foodInput);
+    $(".music").append(musicInput);
+    $(".color").append(colorInput);
 
     $("#answers").show();
 
